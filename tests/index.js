0078333ce7559625731036feec9c92e4754c5580
HARRY DENLEY <HARRY.DENLEY@OUTLOOK.COM>
2021-12-30 00:51:32 +0000

    Initial commit

--- tests/index.js
+++ tests/index.js
@@ -23,11 +23,3 @@ describe('Valid data file', function() {
     });
   })
 });
-
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
